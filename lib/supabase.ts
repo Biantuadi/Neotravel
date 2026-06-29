7131ee698e6ffba2cd6734b1fc570fe43ba1b61a
giovanni <giovanni.fanizza@epitech.eu>
2026-06-29 11:37:16 +0200

    Changements

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -11,9 +11,3 @@ export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
